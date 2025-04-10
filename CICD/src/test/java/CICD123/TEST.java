e7ee64356fe0d8ed240e952d748278a5177ce605
rajesh <bandarurajesh343@gmail.com>
2025-04-10 17:01:37 +0530

    new baseCalss added

--- CICD/src/test/java/CICD123/TEST.java
+++ CICD/src/test/java/CICD123/TEST.java
@@ -1,10 +1,13 @@
 package CICD123;
 
 import org.testng.annotations.Test;
+import Utilites.baseClass;
 
-public class TEST {
-	@Test
-	public static void print() {
-		System.out.println("hello Rajesh");
-	}
+public class TEST extends baseClass {
+
+    @Test
+    public void print() {
+        driver.get("https://mvnrepository.com/artifact/io.github.bonigarcia/webdrivermanager/6.0.1");
+        System.out.println("Website Open Ayindi Rey");
+    }
 }
